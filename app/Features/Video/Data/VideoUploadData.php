8a2e385e2f39ba44930e81be748632d853f31b75
khalidmaquilang <120788000+khalidmaquilang@users.noreply.github.com>
2025-12-22 18:20:05 +0800

    Fix bugs

--- app/Features/Video/Data/VideoUploadData.php
+++ app/Features/Video/Data/VideoUploadData.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Features\Video\Data;
 
-use App\Features\Video\Enums\FeedPrivacyEnum;
+use App\Features\Feed\Enums\FeedPrivacyEnum;
 use Illuminate\Http\UploadedFile;
 use Spatie\LaravelData\Data;
 
