d864a8469d4bf7b04d1f256a02cec100b74319e0
khalidmaquilang <120788000+khalidmaquilang@users.noreply.github.com>
2026-01-07 20:10:43 +0800

    Only shows the live that isn't end yet

--- app/Features/Feed/Actions/GetLiveFeedsAction.php
+++ app/Features/Feed/Actions/GetLiveFeedsAction.php
@@ -7,6 +7,7 @@ namespace App\Features\Feed\Actions;
 use App\Features\Feed\Models\Feed;
 use App\Features\Live\Models\Live;
 use Illuminate\Contracts\Pagination\CursorPaginator;
+use Illuminate\Database\Eloquent\Builder;
 
 class GetLiveFeedsAction
 {
@@ -15,6 +16,7 @@ class GetLiveFeedsAction
         return Feed::query()
             ->with(['user', 'content'])
             ->where('content_type', Live::class)
+            ->whereHas('content', fn (Builder $query) => $query->whereNotNull('ended_at'))
             ->where('user_id', '<>', $user_id)
             ->accessible($user_id)
             ->latest()
