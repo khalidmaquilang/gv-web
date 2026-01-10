d42d6c555b534ed1d3daedc82a06a09cff2ceb14
khalidmaquilang <eskiesiriusmaquilang@gmail.com>
2026-01-11 03:59:59 +0800

    Fix feed query

--- app/Features/Feed/Actions/GetFeedsAction.php
+++ app/Features/Feed/Actions/GetFeedsAction.php
@@ -6,6 +6,7 @@ namespace App\Features\Feed\Actions;
 
 use App\Features\Feed\Models\Feed;
 use App\Features\Live\Models\Live;
+use App\Features\User\Models\User;
 use App\Features\Video\Models\Video;
 use Illuminate\Contracts\Pagination\CursorPaginator;
 use Illuminate\Database\Query\Builder;
@@ -18,7 +19,7 @@ class GetFeedsAction
             ->select('feeds.*')
             // OPTIMIZATION 1: Use a raw subquery with 'exists'.
             // This stops scanning immediately after finding 1 record (faster than count).
-            ->selectRaw('exists(select * from `followers` where `followers`.`following_id` = `feeds`.`user_id` and `followers`.`follower_id` = ?) as is_author_followed_by_user', [$user_id])
+            ->selectRaw('exists(select * from `followables` where `followables`.`followable_id` = `feeds`.`user_id` and `followables`.`followable_type` = ? and `followables`.`user_id` = ?) as is_author_followed_by_user', [User::class, $user_id])
             ->with(['user', 'content'])
             ->accessible($user_id)
             ->where('user_id', '<>', $user_id)
@@ -27,9 +28,10 @@ class GetFeedsAction
             ->when($only_followed, function ($query) use ($user_id): void {
                 $query->whereExists(function (Builder $subQuery) use ($user_id): void {
                     $subQuery->selectRaw(1)
-                        ->from('followers')
-                        ->whereColumn('followers.following_id', 'feeds.user_id')
-                        ->where('followers.follower_id', $user_id);
+                        ->from('followables')
+                        ->whereColumn('followables.followable_id', 'feeds.user_id')
+                        ->where('followables.followable_type', User::class)
+                        ->where('followables.user_id', $user_id);
                 });
             })
             ->whereHasMorph('content', [Video::class, Live::class], function ($query, string $type): void {
